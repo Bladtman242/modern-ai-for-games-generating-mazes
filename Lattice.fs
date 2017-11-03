bcee4ee6150a64f109e0cdc10e858140c4aea8ee
Bladtman <sigurt.bladt@gmail.com>
2017-11-03 16:14:44 +0100

    Add dummy implementation of lattice module

--- Lattice.fs
+++ Lattice.fs
@@ -1,1 +1,18 @@
 module Lattice
+open Block
+
+type LBlock = { b: unit }
+
+type Lat = { b: unit }
+
+type LatPos = { b: unit }
+
+let exits (lb : LBlock) : LatPos list = [{b = ()}]
+
+let neighbours (lat : Lat) (pos : LatPos) : LBlock list = [{b = ()}]
+
+let fitDef (lat : Lat) (pos : LatPos) : ExitVect = Block.exits Block.create
+
+let fit (vect : ExitVect) (b : Block) : LBlock option = Some {b = ()}
+
+let addBlock (lat : Lat) (lb : LBlock) : Lat = {b = () }
