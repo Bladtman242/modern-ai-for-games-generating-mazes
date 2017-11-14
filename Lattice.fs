7c74299d08d96e45f3525ed370278be4c79a5ccc
Bladtman <sigurt.bladt@gmail.com>
2017-11-14 13:32:23 +0100

    Expose (int*int) representation of positions in Lattice

--- Lattice.fs
+++ Lattice.fs
@@ -5,31 +5,28 @@ open OptionExtensions
 
 type Pos = int * int
 
-type LatPos = { pos: Pos }
-
 // An LBlock consists of the "original" block (defining the layout/maze of an
 // area), a position in the lattice (the full maze), and an orientation
 // represented by the number of 90 degree clockwise rotations of the template
 // block
-type LBlock = { template : Block; position : LatPos; orientation : int}
+type LBlock = { template : Block; position : Pos; orientation : int}
 
-type Lat = { lat : Map<LatPos,LBlock> }
+type Lat = { lat : Map<Pos,LBlock> }
 
 let exitVect ({template = b; position = _; orientation = r} : LBlock) : ExitVect =
     Block.rotate r (Block.exits b)
 
-let neighbourhood (p : LatPos) : Neighbourhood<LatPos> =
-    let (x,y) = p.pos
+let neighbourhood ((x,y) : Pos) : Neighbourhood<Pos> =
     {
-        north = { pos = (x,y+1) };
-        east =  { pos = (x+1,y) };
-        south = { pos = (x,y-1) };
-        west =  { pos = (x-1,y) }
+        north = (x,y+1);
+        east =  (x+1,y);
+        south = (x,y-1);
+        west =  (x-1,y)
     }
 
 let emptyLat = { lat = Map.empty }
 
-let exits (lb : LBlock) : LatPos list =
+let exits (lb : LBlock) : Pos list =
     let { template = b; position = lpos; orientation = o } = lb
     let lbExits = Block.rotate o (Block.exits b)
     let neighbourhood = neighbourhood lpos
@@ -43,7 +40,7 @@ let exits (lb : LBlock) : LatPos list =
  |> List.map fst
 
 
-let neighbourhoodLBlocks (lat : Lat) (pos : LatPos) : Neighbourhood<LBlock option> =
+let neighbourhoodLBlocks (lat : Lat) (pos : Pos) : Neighbourhood<LBlock option> =
     let neighbourPositions = neighbourhood pos
     let lBlocks = List.map (fun k -> Map.tryFind k lat.lat) (neighbourPositions.toList)
     { north = List.item 0 lBlocks;
@@ -52,11 +49,11 @@ let neighbourhoodLBlocks (lat : Lat) (pos : LatPos) : Neighbourhood<LBlock optio
       west = List.item 3 lBlocks
     }
 
-let neighbours (lat : Lat) (pos : LatPos) : LBlock list =
+let neighbours (lat : Lat) (pos : Pos) : LBlock list =
     let neighbourPositions = neighbourhoodLBlocks lat pos
     List.collect Option.toList neighbourPositions.toList
 
-let fitDef (lat : Lat) (pos : LatPos) : Neighbourhood<ExitVect option> =
+let fitDef (lat : Lat) (pos : Pos) : Neighbourhood<ExitVect option> =
     // Get the neighbouring blocks' vectors, with proper rotation
     let neighbourExits = neighbourhoodLBlocks lat pos
                       |> Neighbourhood.map (Option.map exitVect)
@@ -75,7 +72,7 @@ let addBlock (lat : Lat) (lb : LBlock) : Lat =
     let newMap = Map.add lb.position lb lat.lat
     { lat = newMap }
 
-let placeBlock (block: Block) (pos : LatPos) (lat : Lat) : Lat option =
+let placeBlock (block: Block) (pos : Pos) (lat : Lat) : Lat option =
     let neighbourVectors = fitDef lat pos
     let possibleFits = Block.fit neighbourVectors block
     if List.isEmpty possibleFits then None
@@ -85,11 +82,8 @@ let placeBlock (block: Block) (pos : LatPos) (lat : Lat) : Lat option =
         orientation = List.head possibleFits
     }
 
-let lBlockAt (pos: LatPos) (lat: Lat) : LBlock option =
+let lBlockAt (pos: Pos) (lat: Lat) : LBlock option =
     Map.tryFind pos lat.lat
-
-let lBlockAtPos (pos : Pos) (lat: Lat) : LBlock option =
-    lBlockAt { pos = pos} lat
 
 let toStrings (lat : Lat) : string list =
     let size = Constants.BlockSize * 2 + 1
@@ -97,7 +91,7 @@ let toStrings (lat : Lat) : string list =
         let row = String.init size (fun _ -> " ")
         List.init size (fun _ -> row)
     let getAsString (p : int * int) : string list =
-        lBlockAtPos p lat
+        lBlockAt p lat
      |> Option.map (fun lb -> Block.print lb.template lb.orientation)
      |> Option.getOrElse emptyBlock
 
@@ -108,7 +102,7 @@ let toStrings (lat : Lat) : string list =
             ]
         List.reduce (List.map2 (+)) blockStrings
 
-    let coords : (int*int) list = Map.toList lat.lat |> List.map fst |> List.map (fun lp -> lp.pos)
+    let coords : (int*int) list = Map.toList lat.lat |> List.map fst
     let minX = List.minBy fst coords |> fst
     let maxX = List.maxBy fst coords |> fst
     let minY = List.minBy snd coords |> snd
--- Lattice.fsi
+++ Lattice.fsi
@@ -5,7 +5,7 @@ open Neighbourhood
 type LBlock
 
 type Lat
-type LatPos
+type Pos = int*int
 
 val emptyLat : Lat
 
@@ -15,13 +15,13 @@ val toStrings : Lat -> string list
 
 // list of lattice positions the LBlock has exits to, regardless of whether or
 // not there are blocks in those positions
-val exits : (LBlock -> LatPos list)
+val exits : (LBlock -> Pos list)
 
 // List of LBlocks in the positions surrounding the given posision
-val neighbours : (Lat -> LatPos -> LBlock list)
-val fitDef : (Lat -> LatPos -> Neighbourhood<ExitVect option>)
+val neighbours : (Lat -> Pos -> LBlock list)
+val fitDef : (Lat -> Pos -> Neighbourhood<ExitVect option>)
 val fits : (Neighbourhood<ExitVect option> -> Lat -> Block -> int list)
-val placeBlock : (Block -> LatPos -> Lat -> Lat option)
+val placeBlock : (Block -> Pos -> Lat -> Lat option)
 val addBlock : (Lat -> LBlock -> Lat)
 
 // vim: set sw=4 ts=4 et:
