5ca0b4c2422ecda0e0e132160c5a38a11650272a
Bladtman <sigurt.bladt@gmail.com>
2017-11-14 15:37:51 +0100

    Use graph in pogram.fs

--- Program.fs
+++ Program.fs
@@ -6,7 +6,12 @@ let main argv =
     System.Console.OutputEncoding <- System.Text.Encoding.UTF8
 
 
-    let g1 = Graph.addEdge (0,0) (1,0) Graph.empty |> Graph.addEdge (0,0) (0,1) |> Graph.addEdge (0,1) (1,1)
+    let g1 = Graph.addEdge (0,0) (1,0) Graph.empty
+          |> Graph.addEdge (0,0) (0,1)
+          |> Graph.addEdge (0,1) (1,1)
+          |> Graph.addEdge (0,1) (0,2)
+          |> Graph.addEdge (0,2) (1,2)
+          |> Graph.addEdge (2,1) (1,1)
 
     Lattice.print <| StructureGraph.toLat g1
     0
