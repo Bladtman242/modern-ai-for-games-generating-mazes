31bfbe67e7af3e9e60fb92b342a8b7fb9c9c80f7
Bladtman <sigurt.bladt@gmail.com>
2017-11-14 17:40:45 +0100

    Change type sig in StructureGraph to closer reflect "concept"
    May or or may not actually improve clarity

--- StructureGraph.fs
+++ StructureGraph.fs
@@ -5,14 +5,14 @@ type LatGen = (int*int) Graph -> Lattice.Lat
 type BlockPicker = Lattice.Pos -> Lattice.Lat -> Lattice.LBlock
 
 let private rnd = System.Random ()
-let private blocks = List.init 10000 (fun _ -> Block.createRandom rnd)
+let private blocks = List.init 200 (fun _ -> Block.createRandom rnd)
 
 let private rndFit (p : Lattice.Pos) (l: Lattice.Lat) =
     let fits = List.filter (fun b -> Option.isSome <| Lattice.placeBlock b p l) blocks
     let b = List.item (rnd.Next (List.length fits)) fits
     Lattice.LBlock b (List.head <| Lattice.fits (Lattice.fitDef l p) l b) p
 
-let toLat (f: BlockPicker) (g: (int*int) Graph) : Lattice.Lat =
+let toLat : BlockPicker -> LatGen = fun f g ->
     let nodeSet = Graph.nodes g
     Set.fold (fun l n -> Lattice.addBlock (f n l) l) Lattice.emptyLat nodeSet
 
